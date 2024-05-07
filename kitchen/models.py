60ba6dc48cfb77944f052c2e6e524dbdd845b227
skeletronys <mykytcey@gmail.com>
2024-05-07 20:50:00 +0200

    add to models.py, Dish and Cook

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,5 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class DishType(models.Model):
     name = models.CharField(max_length=255)
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook)
