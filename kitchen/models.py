d0ca822abdf4d2ebfa9843ddef5467a3898a67e3
skeletronys <mykytcey@gmail.com>
2024-05-08 02:55:21 +0200

    upload models.py add related_name and verbose_name

--- kitchen/models.py
+++ kitchen/models.py
@@ -13,12 +13,16 @@ class Ingredient(models.Model):
 class Cook(AbstractUser):
     years_of_experience = models.IntegerField()
 
+    class Meta:
+        verbose_name = 'cook'
+        verbose_name_plural = 'cooks'
+
 
 class Dish(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    ingredients = models.ManyToManyField(Ingredient)
+    ingredients = models.ManyToManyField(Ingredient, related_name='ingredient_dishes')
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name='type_dishes')
     image = models.ImageField(upload_to='dishes/')
-    cooks = models.ManyToManyField(Cook)
+    cooks = models.ManyToManyField(Cook, related_name='cooked_dishes')
