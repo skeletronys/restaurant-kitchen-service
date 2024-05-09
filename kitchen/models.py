0943d6c4f31f47fdf956731915473146dfd5cacf
skeletronys <mykytcey@gmail.com>
2024-05-09 02:59:35 +0200

    upload add to Ingredients and Dish type __str__ (return name)

--- kitchen/models.py
+++ kitchen/models.py
@@ -5,9 +5,15 @@ from django.db import models
 class DishType(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class Cook(AbstractUser):
