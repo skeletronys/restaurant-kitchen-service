dc73f58452ae3096e5a473dc1f6aefc1fddd04d4
skeletronys <mykytcey@gmail.com>
2024-05-10 02:44:11 +0200

    fix logged_out

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -131,6 +131,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
+LOGIN_REDIRECT_URL = "/"
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'kitchen.Cook'
