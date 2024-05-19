2ece95b6229fd9c17520cbd535d8f2ffb702bcdb
skeletronys <mykytcey@gmail.com>
2024-05-19 23:27:40 +0200

    Update: settings for deploying

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'fallback_secret_key')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_KEY', "") != False
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
